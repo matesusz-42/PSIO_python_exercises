e484d5b3a1f785f2eea4e85a5ceda2b1a2a288fc
Mateusz <mati.walczak26@gmail.com>
2026-05-30 12:28:15 +0200

    .

--- skeletons/catalogue-1/catalogue.py
+++ skeletons/catalogue-1/catalogue.py
@@ -5,31 +5,35 @@ from typing import Dict, Any, Optional
 
 
 class Product:
-    def __init__(self, id_: str, name: str, price: float) -> None:
-        # TODO: Zaimplementuj.
-        
-        self.id = id
+    def __init__(self, id_ : str, name : str, price : float):
+        self.id = id_
         self.name = name
         self.price = price
     def __str__(self) -> str:
-        return f"{self.name} [{self.id}]: ${self.price:.2f}"
-    def __eq__(self, other: Any) -> bool:
+        return f"{self.nazwa} [{self.id}] : ${self.price:.2f}"
+    def __eq__(self, other : Any) -> bool:
         if not isinstance(other, Product):
             return False
         return self.id == other.id and self.name == other.name and self.price == other.price
+
+
         
 
 
+Inventory = Dict[str, Product]
+        
 class Catalogue:
-    # TODO: Zaimplementuj.
-     Inventory = Dict[str, Product]
-     def __init__(self, inventory:Inventory = None) -> None:
+    Inventory = Dict[str, Product]
+    def __init__(self, inventory: Optional[Inventory] = None) -> None:
         if inventory is None:
-            self.inventory: Catalogue.Inventory = {}
+            self.inventory : Inventory = {}
         else:
-            self.inventory: Catalogue.Inventory == copy.deepcopy(inventory)
-    def add_product(self, product: Product) -> None:
-        product_copy = copy.deepcopy(product)
-        self.inventory[product_copy.id] = product_copy
-    def __contains__(self, id : str) -> bool:
-        return id in self.inventory       
+            self.inventory = invertory.copy()
+        
+    def add_product(self, product:Product) -> None:
+        self.inventory[product.id] = product
+        print(f"[katalog] dodano produkt : {product.name}")
+    def __contains__(self, id_ : str) -> bool:
+        return id_ in self.inventory
+
+     
